ff227258735c6d627ea3fea517be9007cfb4a53a
Tymoteusz Lango <58483383+tymek805@users.noreply.github.com>
2025-01-23 15:53:17 +0100

    Optimize imports

--- ui/main_componenets.py
+++ ui/main_componenets.py
@@ -2,7 +2,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
     QWidget, QLabel, QPushButton, QVBoxLayout,
-    QHBoxLayout, QFrame, QApplication, QSpacerItem, QSizePolicy, QTextEdit, QScrollArea
+    QHBoxLayout, QFrame, QSizePolicy, QScrollArea
 )
 
 
